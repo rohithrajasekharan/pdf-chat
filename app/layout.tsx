8f927d38aa54abb593d2ea26e303686338e765b9
rohithrajasekharan <rohithrajasekharan@outlook.com>
2024-07-20 23:20:14 +0530

    react query set up

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Providers from "./components/Providers";
+import { Toaster } from "react-hot-toast"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +20,12 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Providers>
+            {children}
+            <Toaster />
+          </Providers>
+        </body>
       </html>
     </ClerkProvider>
   );
