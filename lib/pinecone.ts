812ccc1d9c9bc6596ae707192d71bbe4bcdb61bc
rohithrajasekharan <rohithrajasekharan@outlook.com>
2024-07-20 23:22:12 +0530

    langchain and pinecone db setup

---
+++ lib/pinecone.ts
@@ -0,0 +1,24 @@
+import { Pinecone } from '@pinecone-database/pinecone'
+import { downloadFromS3 } from './s3-server';
+import {PDFLoader} from 'langchain/document_loaders/fs/pdf'
+
+let pinecone: Pinecone | null = null;
+
+export const getPineconeClient = async () => {
+    if(!pinecone) {
+        pinecone = new Pinecone({
+            apiKey: process.env.PINECONE_API_KEY!
+        });
+        
+        return pinecone
+    }
+}
+
+export async function loadS3IntoPinecon(file_key:string) {
+        console.log("downloading s3 into file system")
+        const file_name = await downloadFromS3(file_key);
+        if (!file_name) {
+            throw new Error("could not download from s3")
+        }
+        const loader = new PDFLoader(file_name)
+}
